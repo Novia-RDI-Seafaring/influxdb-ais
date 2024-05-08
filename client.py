c395649c33eccfd4fca214ab1afedcc0f077f310
mmanngard <mmanngar@abo.fi>
2024-05-08 22:40:49 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import uuid
 import paho.mqtt.client as mqtt
 import time
+import json
 
 # This is an example client for retrieving AIS data from the Digitraffic API
 # https://www.digitraffic.fi/en/marine-traffic/
@@ -9,11 +10,20 @@ APP_NAME = 'Test/TestApp'
 TOPIC = 'vessels-v2/#'
 
 #########
-
+def format_message(message):
+    topic_hierarchy = str(message.topic).split("/")
+    print(topic_hierarchy)
+    id = topic_hierarchy[1]
+    topic = topic_hierarchy[2]
+    payload = message.payload.decode('utf-8')
+    return id, topic, payload
+    
 def on_message(client, userdata, message):
-    print('Topic:', str(message.topic))
-    print('Payload:',  str(message.payload.decode('utf-8')))
-    print('----')
+    id, topic, payload = format_message(message)
+    print("MMSI : " + id)
+    print("Topic : " + topic)
+    print("Payload : " + payload)
+    print("----")
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -33,6 +43,6 @@ client.tls_set()
 client.connect('meri.digitraffic.fi', 443)
 
 client.loop_start()
-time.sleep(60)
+time.sleep(10)
 client.loop_stop()
 client.disconnect()
